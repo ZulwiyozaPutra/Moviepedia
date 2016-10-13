4ac9dc7ba815f25096894b61a41411b033525061
Zulwiyoza Putra <zulwiyozaputra@yahoo.com>
2016-10-13 01:11:32 -0700

    Initial Commit

---
+++ Moviepedia/Moviepedia/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Moviepedia
+//
+//  Created by Zulwiyoza Putra on 10/13/16.
+//  Copyright © 2016 Zulwiyoza Putra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "")
+        cell.textLabel?.text = "Row \(indexPath.row)"
+        return cell
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
